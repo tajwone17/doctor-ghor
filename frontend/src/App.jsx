1a117f7adff35e22e9d57e7e3f230ee884d44db5
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-02 00:39:17 +0600

    add routing and page components for Home, About, Contact, Doctors, Login, MyProfile, MyAppointments, and Appointment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Doctors from './pages/Doctors';
 const App = () => {
   return (
-    <div>
-      <p className='text-green-'>Hello, World!</p>
+    <div className='mx-4 sm:mx-[10%]'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+      </Routes>
     </div>
   )
 }
