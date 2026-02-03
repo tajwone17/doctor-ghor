12b566465e59f8b168f32f1f435c9c52f2dfda36
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-04 01:37:28 +0600

    add Footer component and integrate into App; enhance layout and content for better user experience

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { assets } from "../assets/assets_frontend/assets";
+const Footer = () => {
+  return (
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        {/* -----Left Section----- */}
+        <div>
+          <img src={assets.logo} alt="logo" className="mb-5 w-40" />
+
+          <p className="text-gray-600 w-full md:w-2/3 leading-6">
+            Welcome to our website, your go-to platform for all your healthcare
+            needs. We provide comprehensive information and resources to help
+            you make informed decisions about your health. Book appointments,
+            explore specialties, and connect with trusted doctors with ease.
+          </p>
+        </div>
+        {/* -----Center Section----- */}
+        <div>
+          <p className="text-xl font-medium mb-5">COMPANY</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        {/* -----Right Section----- */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+8801234567891</li>
+            <li>doc@doctorghor.com</li>
+          </ul>
+        </div>
+      </div>
+      {/* ---------Copyright Section--------- */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">Copyright 2026@ Doctor Ghor - All Rights Reserved.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
