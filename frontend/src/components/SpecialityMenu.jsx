87f2e41b5cb9aa889aedce82dbc98699715e8418
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-03 19:56:29 +0600

    add SpecialityMenu component with dynamic data and integrate into Home page

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { specialityData } from "../assets/assets_frontend/assets/";
+import { Link } from "react-router-dom";
 const SpecialityMenu = () => {
   return (
-    <div>
-      
+    <div id="speciality" className="flex flex-col gap-4 py-16 text-gray-800 items-center ">
+      <h1 className="text-3xl font-medium">Find by Speciality</h1>
+      <p className="sm:w-1/3 text-center text-sm">
+        Simply browse through our extensive list of trusted doctors, schedule
+        your appointment hassle-free.
+      </p>
+      <div className="flex sm:justify-center gap-4 pt-5 w-full overflow-scroll">
+        {specialityData.map((item, index) => (
+          <Link onClick={()=>(scrollTo(0,0))} key={index} to={`/doctors/${item.speciality}`} className="flex flex-col items-center text-xs shrink-0 cursor-pointer hover:-translate-y-2.5 transition-all duration-500">
+            <img src={item.image} alt={item.name} className="w-16 mb-2 sm:w-24"/>
+            <p>{item.speciality}</p>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
