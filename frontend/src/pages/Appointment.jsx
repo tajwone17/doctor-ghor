0a1abf3e4c519dc77b5ce8dca157a64b8854fa45
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-05 14:41:31 +0600

    enhance Appointment component to include dynamic booking slots and display appointment fee; refactor slot grouping logic for improved functionality

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,11 +1,75 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContextInstance";
 import { assets } from "../assets/assets_frontend/assets";
 const Appointment = () => {
+  const daysOfWeek = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
   const { docId } = useParams();
-  const { doctors,currencySymbol } = useContext(AppContext);
+  const { doctors, currencySymbol } = useContext(AppContext);
   const [doctorInfo, setDoctorInfo] = useState(null);
+  const docSlot = useMemo(() => {
+    if (!doctorInfo) return [];
+    const slots = [];
+    //getting current date
+    let today = new Date();
+    for (let i = 0; i < 7; i++) {
+      //getting date with index
+      let currentDate = new Date(today);
+      currentDate.setDate(today.getDate() + i);
+      // setting end time of the date with index
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+      // Setting hours
+      if (today.getDate() === currentDate.getDate()) {
+        currentDate.setHours(
+          currentDate.getHours() > 10 ? currentDate.getHours() + 1 : 10,
+          0,
+          0,
+          0,
+        );
+        currentDate.setMinutes(currentDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currentDate.setHours(10);
+        currentDate.setMinutes(0);
+      }
+      let timeSlots = [];
+      while (currentDate < endTime) {
+        let formattedTime = currentDate.toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        });
+        //add slot to array
+        timeSlots.push({
+          dateTime: new Date(currentDate),
+          formattedTime: formattedTime,
+        });
+        //Increment by 30 minutes
+        currentDate.setMinutes(currentDate.getMinutes() + 30);
+      }
+      slots.push(...timeSlots);
+    }
+    return slots;
+  }, [doctorInfo]);
+  const [slotIndex, setSlotIndex] = useState(0);
+ 
+
+  // Group slots by date
+  const groupedSlots = useMemo(() => {
+    const grouped = {};
+    docSlot.forEach((slot) => {
+      const dateKey = slot.dateTime.toDateString();
+      if (!grouped[dateKey]) {
+        grouped[dateKey] = {
+          date: slot.dateTime,
+          times: [],
+        };
+      }
+      grouped[dateKey].times.push(slot);
+    });
+    return Object.values(grouped);
+  }, [docSlot]);
+
   const fetchDocInfo = async () => {
     const doctor = doctors.find((doc) => doc._id === docId);
     setDoctorInfo(doctor);
@@ -14,7 +78,9 @@ const Appointment = () => {
     //eslint-disable-next-line
     fetchDocInfo();
   }, [docId, doctors]);
-
+  useEffect(() => {
+    console.log(docSlot);
+  }, [docSlot]);
   return (
     doctorInfo && (
       <div>
@@ -31,24 +97,61 @@ const Appointment = () => {
             {/* ------Doctor Info: Name,Degree,Experience------ */}
             <p className="flex items-center gap-2 md:text-5xl text-3xl font-medium text-gray-900">
               {doctorInfo?.name}{" "}
-              <img className="w-5" src={assets.verified_icon} alt="verified" />{" "}
+              <img
+                className="w-5"
+                src={assets.verified_icon}
+                alt="verified"
+              />{" "}
             </p>
             <div className="flex items-center gap-2 md:text-2xl text-xl mt-1 text-gray-600">
               <p>
                 {doctorInfo?.degree} - {doctorInfo?.speciality}
               </p>
-              <button className="py-0.5 px-2 border text-sm rounded-full">{doctorInfo?.experience}</button>
+              <button className="py-0.5 px-2 border text-sm rounded-full">
+                {doctorInfo?.experience}
+              </button>
             </div>
             {/* -----Doctor About----- */}
             <div>
-            <p className="flex items-center gap-1 md:text-2xl text-xl font-medium text-gray-900 mt-3">
-              About <img src={assets.info_icon} alt="info" />
+              <p className="flex items-center gap-1 md:text-2xl text-xl font-medium text-gray-900 mt-3">
+                About <img src={assets.info_icon} alt="info" />
+              </p>
+              <p className="md:text-xl text-lg text-gray-500 max-w-175 mt-1">
+                {doctorInfo?.about}
+              </p>
+            </div>
+            <p className="text-2xl font-semibold text-gray-500 mt-4">
+              Appointment Fee:{" "}
+              <span className="text-gray-600">
+                {currencySymbol}
+                {doctorInfo?.fees}
+              </span>
             </p>
-            <p className="md:text-xl text-lg text-gray-500 max-w-175 mt-1">{doctorInfo?.about}</p>
           </div>
-          <p className="text-2xl font-semibold mt-4">Appointment Fee: <span>{currencySymbol}{doctorInfo?.fees}</span></p>
         </div>
-      </div>
+        {/* Booking slot */}
+        <div className="lg:ml-103 sm:pl-4 mt-4 font-medium text-gray-700">
+          <p>Booking Slots</p>
+          <div className="flex gap-3 items-center w-full overflow-x-scroll mt-4">
+            {groupedSlots.length > 0 &&
+              groupedSlots.map((daySlots, index) => (
+                <div
+                  onClick={() => setSlotIndex(index)}
+                  className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ? "bg-primary text-white " : "border border-gray-200"}`}
+                  key={index}
+                >
+                  <p>{daysOfWeek[daySlots.date.getDay()]} </p>
+                  <p>{daySlots.date.getDate()} </p>
+                </div>
+              ))}
+          </div>
+          <div>
+            {groupedSlots.length > 0 &&
+              groupedSlots[slotIndex].times.map((slot, index) => (
+                <p key={index}>{slot.formattedTime}</p>
+              ))}
+          </div>
+        </div>
       </div>
     )
   );
