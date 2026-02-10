0a697ca89e6d479e12773c2b0131d53e0407386b
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-11 00:10:21 +0600

    feat: implement MongoDB and Cloudinary connection; add user and doctor models

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
 import 'dotenv/config';
-
+import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 //app config
 const app = express(); 
 const port = process.env.PORT || 4000; 
 
+//connect to database
+connectDB();
+connectCloudinary();
 //middlewares
 app.use(cors());
 app.use(express.json());
