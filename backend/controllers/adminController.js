6d9133c904792c01665e05538429bff0f9c07772
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-14 00:47:35 +0600

    feat: add admin login functionality; update routes and environment variables

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from "validator";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import DoctorModel from "../models/doctorModel.js";
+import JWT from "jsonwebtoken";
 //api for adding doctors
 const addDoctor = async (req, res) => {
   try {
@@ -79,4 +80,28 @@ const addDoctor = async (req, res) => {
     res.status(500).send({ message: "Error adding doctor", error });
   }
 };
-export { addDoctor };
+
+//api for admin login
+const adminLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ success: false, message: "All fields are required" });
+    }
+    if (
+      email !== process.env.ADMIN_EMAIL ||
+      password !== process.env.ADMIN_PASSWORD
+    ) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+    const token=JWT.sign({email,password},process.env.JWT_SECRET_KEY);
+    res.json({ success: true, message: "Admin login successful", token });
+  } catch (error) {
+    console.error("Error during admin login:", error);
+    res
+      .status(500)
+      .json({ success: false, message: "Error during admin login", error });
+  }
+};
+
+export { addDoctor, adminLogin };
--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { addDoctor } from "../controllers/adminController.js";
+import { addDoctor,adminLogin } from "../controllers/adminController.js";
 import upload from "../middlewares/multer.js";
 
 const adminRouter = express.Router();
 
 adminRouter.post("/add-doctor", upload.single("image"), addDoctor);
+adminRouter.post("/admin-login", adminLogin);
 export default adminRouter;
