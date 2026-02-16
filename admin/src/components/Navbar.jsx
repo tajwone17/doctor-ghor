dc591205b249bc3a6697085ac5807db2385d8bf6
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-02-17 00:44:01 +0600

    feat: enhance AddDoctor component with form fields for doctor details and image upload

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { React, useContext } from "react";
 import { assets } from "../assets/assets";
 import { AdminContext } from "../context/AdminContextInstance";
+import { NavLink } from "react-router-dom";
 const Navbar = () => {
   const { aToken, setAtoken } = useContext(AdminContext);
   const handleLogout = () => {
@@ -11,11 +12,14 @@ const Navbar = () => {
   return (
     <div className="flex justify-between items-center bg-white px-4 py-3 sm:px-10 border-b">
       <div className="flex items-center gap-2 text-xs">
+        <NavLink to="/" >
         <img
           className="w-40 sm:w-45 cursor-pointer"
           src={assets.admin_logo}
           alt=""
+
         />
+        </NavLink>
         <p className="border px-2.5 py-0.5 text-gray-600 rounded-full border-gray-500">
           {aToken ? "Admin" : "Doctor"}
         </p>
--- admin/src/pages/admin/AddDoctor.jsx
+++ admin/src/pages/admin/AddDoctor.jsx
@@ -1,11 +1,132 @@
-import React from 'react'
-
+import React from "react";
+import { assets } from "../../assets/assets";
 const AddDoctor = () => {
-  return (
-    <div>
-      add
-    </div>
-  )
-}
 
-export default AddDoctor
+
+
+  
+  return (
+    <form className="m-5 w-full">
+      <p className="text-lg font-medium mb-3">Add Doctor</p>
+      <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl overflow-y-scroll">
+        <div className="flex items-center gap-4 mb-8 text-gray-500">
+          <label htmlFor="doc-img">
+            <img className="w-16 bg-gray-100 rounded-full cursor-pointer" src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="doc-img" className="hidden" />
+          <p>
+            Upload Doctor <br /> Image
+          </p>
+        </div>
+        <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Name</p>
+              <input
+                className=" border border-gray-300 rounded px-3 py-2"
+                type="text"
+                placeholder="Enter Doctor Name"
+                required
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Email</p>
+              <input
+                className=" border border-gray-300 rounded px-3 py-2"
+                type="email"
+                placeholder="Enter Doctor Email"
+                required
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor Password</p>
+              <input
+                className=" border border-gray-300 rounded px-3 py-2"
+                type="password"
+                placeholder="Enter Doctor Password"
+                required
+              />
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Doctor`s Experience</p>
+              <select name="" id="" className=" border-gray-300 border rounded px-3 py-2">
+                <option value="1 Year">1 Year</option>
+                <option value="2 Years">2 Years</option>
+                <option value="3 Years">3 Years</option>
+                <option value="4 Years">4 Years</option>
+                <option value="5 Years">5 Years</option>
+                <option value="6 Years">6 Years</option>
+                <option value="7 Years">7 Years</option>
+                <option value="8 Years">8 Years</option>
+                <option value="9 Years">9 Years</option>
+                <option value="10+ Years">10+ Years</option>
+              </select>
+            </div>
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Fees</p>
+              <input
+                className=" border border-gray-300 rounded px-3 py-2"
+                type="number"
+                placeholder="Enter Doctor Fees"
+                required
+              />
+            </div>
+          </div>
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
+              <p>Speciality</p>
+              <select name="" id="" className=" border border-gray-300 rounded px-3 py-2">
+                <option value="General Physician">General Physician</option>
+                <option value="Dermatologist">Dermatologist</option>
+                <option value="Neurologist">Neurologist</option>
+                <option value="Pediatrician">Pediatrician</option>
+                <option value="Gynecologist">Gynecologist</option>
+                <option value="Gastroenterologist">Gastroenterologist</option>
+              </select>
+            </div>
+            <div>
+          
+                <p>Education</p>
+                <input
+                   className="w-full border-gray-300 border rounded px-3 py-2"
+                  type="text"
+                  placeholder="Enter Doctor Education"
+                  required
+                />
+              </div>
+
+              <div className="flex-1 flex flex-col gap-1">
+                <p>Address</p>
+                <input
+                  className=" border border-gray-300 rounded px-3 py-2"
+                  type="text"
+                  placeholder="Enter Doctor Address 1"
+                  required
+                />
+                <input
+                 className=" border border-gray-300 rounded px-3 py-2"
+                  type="text"
+                  placeholder="Enter Doctor Address 2"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <p className="mt-4 mb-2">About Doctor</p>
+            <textarea
+              className=" w-full px-4 pt-2 rounded border border-gray-300"
+              placeholder="Write about doctor"
+              required
+              rows={5}
+            />
+          </div>
+          <button className="bg-primary text-white mt-4 px-10 py-3 rounded-full">Add Doctor</button>
+        </div>
+
+    </form>
+  );
+};
+
+export default AddDoctor;
