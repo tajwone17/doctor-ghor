29dc1cfde238fd964671ced35db1a2df7669b6b1
Jakaria Chowdhury Tajwone <0562310005101031@neub.edu.bd>
2026-03-25 21:52:59 +0600

    feat: add appointment model and API for booking appointments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 import User from "../models/userModel.js";
+import Doctor from "../models/doctorModel.js";
+import Appointment from "../models/appointmentModel.js";
 import bcrypt from "bcrypt";
 import validator from "validator";
 import JWT from "jsonwebtoken";
@@ -122,6 +124,57 @@ const updateUserDetails = async (req, res) => {
       await User.findByIdAndUpdate(userId, { image: imageUrl });
     }
     res.json({ success: true, message: "User details updated successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Server error" });
+    console.log(error);
+  }
+};
+
+//API to book an appointment
+const bookAppointment = async (req, res) => {
+  try {
+    const { userId, docId, slotDate, slotTime } = req.body;
+    const docData = await Doctor.findById(docId).select("-password");
+
+    if (!docData.available) {
+      return result.json({
+        success: false,
+        message: "Doctor is not Available",
+      });
+    }
+    let slots_booked = docData.slots_booked;
+
+    //checking for slot availability
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({ success: false, message: "Slot is already booked" });
+      } else {
+        slots_booked[slotDate].push(slotTime);
+      }
+    } else {
+      slots_booked[slotDate] = [];
+      slots_booked[slotDate].push(slotTime);
+    }
+    const userData = await User.findById(userId).select("-password");
+    delete docData.slots_booked;
+    const appointmentData = {
+      userId,
+      docId,
+      userData,
+      docData,
+      amount: docData.fees,
+      slotDate,
+      slotTime,
+      date: Date.now(),
+    };
+    const newAppointment = new Appointment(appointmentData);
+    await newAppointment.save();
+    await Doctor.findByIdAndUpdate(docId, { slots_booked });
+    res.json({
+      success: true,
+      message: "Appointment booked successfully",
+      appointmentData,
+    });
   } catch (error) {
     res.status(500).json({ message: "Server error" });
     console.log(error);
